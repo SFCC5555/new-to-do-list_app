8810a5002a3ea4e5781fe8d86d3e52e65b1a054d
sfcc5555 <sfcc.5555@gmail.com>
2024-02-24 19:10:20 -0500

    update

--- server/src/app.js
+++ server/src/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 const authRoutes = require("./routes/auth.routes");
 const tasksRoutes = require("./routes/tasks.routes");
 
-const notFound = require("./middlewares/notFound");
+//const notFound = require("./middlewares/notFound");
 
-const corsOptions = require("./libs/corsOptions");
+//const corsOptions = require("./libs/corsOptions");
 const config = require("./config");
 
 const app = express();
@@ -16,7 +17,7 @@ const app = express();
 // Middlewares
 
 //app.use(cors());
-app.use(cors(corsOptions(config.allowedOrigins))); // Apply CORS middleware with configured options
+//app.use(cors(corsOptions(config.allowedOrigins))); // Apply CORS middleware with configured options
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -25,6 +26,14 @@ app.use(cookieParser());
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", tasksRoutes);
 
-app.use(notFound);
+// Servir archivos estáticos desde 'client'
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
+  
+
+//app.use(notFound);
 
 module.exports = app;
